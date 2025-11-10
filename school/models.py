566f62f546c51088629259d12c72c444c034004d
Gabriel <--add>
2025-11-10 08:22:08 -0300

    Salvando alterações antes do merge

--- school/models.py
+++ school/models.py
@@ -235,7 +235,7 @@ class Nota(models.Model):
         (3, '3º Bimestre'),
         (4, '4º Bimestre'),
     ]
-    bimestre = models.PositiveSmallIntegerField(choices=BIMESTRE_CHOICES, verbose_name='Bimestre')
+    bimestre = models.PositiveSmallIntegerField(choices=BIMESTRE_CHOICES, verbose_name='Bimestre', null=True)
 
     # Valor da nota
     nota = models.DecimalField(max_digits=5, decimal_places=2)
